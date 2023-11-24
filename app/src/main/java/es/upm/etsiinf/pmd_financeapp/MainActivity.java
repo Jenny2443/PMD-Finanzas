99e02cb7877134ded32f08b22d25ac8144a6dca3
Jennifer <j.zhao@alumnos.upm.es>
2023-11-24 21:23:00 +0100

    cambiado a bottomnavigationview pero no he terminado en mainactiviry

--- app/src/main/java/es/upm/etsiinf/pmd_financeapp/MainActivity.java
+++ app/src/main/java/es/upm/etsiinf/pmd_financeapp/MainActivity.java
@@ -7,10 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button btnStocks;
-    Button btnHome;
-    Button btnHistorial;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity{
+//    Button btnStocks;
+//    Button btnHome;
+//    Button btnHistorial;
+
+    private NavigationBarView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,28 +23,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         //Inicializacion de botones
-        btnStocks = findViewById(R.id.main_btn_stocks);
-        btnHome = findViewById(R.id.main_btn_home);
-        btnHistorial = findViewById(R.id.main_btn_historial);
+//        btnStocks = findViewById(R.id.main_btn_stocks);
+//        btnHome = findViewById(R.id.main_btn_home);
+//        btnHistorial = findViewById(R.id.main_btn_historial);
+
+        //Inicializacion de bottom navigation view
+        bottomNavigationView = findViewById(R.id.main_btn_nav);
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+        // Perform item selected listener
+        bottomNavigationView.
+
+        bottomNavigationView.setOnClickListener(this);
 
         //Asignacion de listener
-        btnStocks.setOnClickListener(this);
-        btnHome.setOnClickListener(this);
-        btnHistorial.setOnClickListener(this);
+//        btnStocks.setOnClickListener(this);
+//        btnHome.setOnClickListener(this);
+//        btnHistorial.setOnClickListener(this);
 
     }
 
-    @Override
-    public void onClick(View v){
-        if(v.getId() == R.id.main_btn_stocks){
-            //Lanzamos la notificacion del boton high
-            Toast.makeText(this, "Boton stocks", Toast.LENGTH_SHORT).show();
-        }else if(v.getId() == R.id.main_btn_home){
-            //Lanzamos la notificacion del boton high
-            Toast.makeText(this, "Boton home", Toast.LENGTH_SHORT).show();
-        }else if(v.getId() == R.id.main_btn_historial){
-            //Lanzamos la notificacion del boton high
-            Toast.makeText(this, "Boton historial", Toast.LENGTH_SHORT).show();
-        }
-    }
+
 }
