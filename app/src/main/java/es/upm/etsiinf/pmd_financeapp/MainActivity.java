6060abdffa63a8ae1a61468774af3bfbc3704501
Jennifer <j.zhao@alumnos.upm.es>
2023-11-25 11:50:51 +0100

    bottomo navigation bar hecho. Por ahora al hacer click te sale resaltado y te sale un toast

--- app/src/main/java/es/upm/etsiinf/pmd_financeapp/MainActivity.java
+++ app/src/main/java/es/upm/etsiinf/pmd_financeapp/MainActivity.java
@@ -1,10 +1,14 @@
 package es.upm.etsiinf.pmd_financeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,7 +19,9 @@ public class MainActivity extends AppCompatActivity{
 //    Button btnHome;
 //    Button btnHistorial;
 
-    private NavigationBarView bottomNavigationView;
+    //private NavigationBarView bottomNavigationView;
+    private BottomNavigationView bottomNavigationView;
+    private TextView textoPrueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +35,27 @@ public class MainActivity extends AppCompatActivity{
 
         //Inicializacion de bottom navigation view
         bottomNavigationView = findViewById(R.id.main_btn_nav);
-        bottomNavigationView.setSelectedItemId(R.id.action_home);
-        // Perform item selected listener
-        bottomNavigationView.
-
-        bottomNavigationView.setOnClickListener(this);
-
+        textoPrueba = findViewById(R.id.main_texto_prueba);
+        //bottomNavigationView.setSelectedItemId(R.id.action_home);
         //Asignacion de listener
 //        btnStocks.setOnClickListener(this);
 //        btnHome.setOnClickListener(this);
 //        btnHistorial.setOnClickListener(this);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.action_home) {
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.action_stocks) {
+                    Toast.makeText(MainActivity.this, "Stocks", Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.action_history) {
+                    Toast.makeText(MainActivity.this, "Historial", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
 
     }
 
