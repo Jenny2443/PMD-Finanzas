514178eb2098db214699c0fde3b249fd08fa3ecd
Jennifer <j.zhao@alumnos.upm.es>
2023-12-10 16:11:13 +0100

    con notificacion basica

--- app/src/main/java/es/upm/etsiinf/pmd_financeapp/TestJobServiceStock.java
+++ app/src/main/java/es/upm/etsiinf/pmd_financeapp/TestJobServiceStock.java
@@ -1,55 +1,55 @@
-//package es.upm.etsiinf.pmd_financeapp;
-//
-//import android.app.Notification;
-//import android.app.NotificationChannel;
-//import android.app.NotificationManager;
-//import android.app.PendingIntent;
-//import android.app.job.JobParameters;
-//import android.app.job.JobService;
-//import android.content.Context;
-//import android.content.Intent;
-//import android.graphics.Color;
-//import android.os.AsyncTask;
-//import android.os.Build;
-//import android.os.Debug;
-//import android.util.Log;
-//
-//import androidx.annotation.RequiresApi;
-//import androidx.core.app.NotificationCompat;
-//
-//import java.io.IOException;
-//
+package es.upm.etsiinf.pmd_financeapp;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Debug;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import java.io.IOException;
+
 //import es.upm.etsiinf.pmd_financeapp.Util.StockJobUtil;
-//
-//public class TestJobServiceStock extends JobService {
-//
-//    @RequiresApi(api = Build.VERSION_CODES.P)
-//    @Override
-//    public boolean onStartJob(JobParameters params) {
-//        Log.i("TestJobServiceStock", "Servicio ejecutado: " );
-//        actualizarAPI(params, getApplicationContext());
-//        Log.i("TestJobServiceStock", "Servicio reprogramado: " );
-//        return true;
-//    }
-//
-//    //Funcion para llamar a la API y actualizar datos a traves de otro thread
-//    private void actualizarAPI(JobParameters jobParameters, Context context) {
-//        Log.i("TestJobService", "Actualizando API");
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    StockManager.updateStocks();
-//                    makeNotification();
-//                    //Ponemos que se vuelva a ejecutar el servicio
-//                    jobFinished(jobParameters, true);
-//                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        }).start();
-//    }
-//
+
+public class TestJobServiceStock extends JobService {
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        Log.i("TestJobServiceStock", "Servicio ejecutado: " );
+        actualizarAPI(params, getApplicationContext());
+        Log.i("TestJobServiceStock", "Servicio reprogramado: " );
+        return true;
+    }
+
+    //Funcion para llamar a la API y actualizar datos a traves de otro thread
+    private void actualizarAPI(JobParameters jobParameters, Context context) {
+        Log.i("TestJobService", "Actualizando API");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    StockManager.updateStocks();
+                    //makeNotification();
+                    //Ponemos que se vuelva a ejecutar el servicio
+                    jobFinished(jobParameters, true);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+
 //    private void makeNotification(){
 //        Log.i("TestJobService", "Notificacion");
 //        String chanelID = "CHANNEL_ID";
@@ -84,10 +84,10 @@
 //        }
 //        notificationManager.notify(0, builder.build());
 //    }
-//
-//    @Override
-//    public boolean onStopJob(JobParameters params) {
-//        return false;
-//    }
-//
-//}
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+
+}
